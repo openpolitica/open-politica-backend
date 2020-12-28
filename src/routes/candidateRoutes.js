68ae32ccc86cf1bfa41e9db6a3802afe3b46b8e9
gianpietro1 <gianpietro1@gmail.com>
2020-12-28 16:25:50 -0500

    Updates schema and per ID routes

--- src/routes/candidateRoutes.js
+++ src/routes/candidateRoutes.js
@@ -6,11 +6,14 @@ const Candidate = mongoose.model("Candidate");
 const router = express.Router();
 
 router.get("/candidates", async (req, res) => {
-  const { page = 1, limit = 10, party } = req.query;
+  const { page = 1, limit = 10, party, role } = req.query;
 
   let query = {};
   if (party) {
     query.org_politica_nombre = party;
+  }
+  if (role) {
+    query.cargo_nombre = role;
   }
 
   const candidates = await Candidate.find(query)
@@ -25,6 +28,20 @@ router.get("/candidates", async (req, res) => {
     totalPages: Math.ceil(count / limit),
     currentPage: page,
   });
+});
+
+router.get("/candidates/hoja_vida_id/:hoja_vida_id", async (req, res) => {
+  const candidate = await Candidate.findOne({
+    hoja_vida_id: req.params.hoja_vida_id,
+  });
+  res.send(candidate);
+});
+
+router.get("/candidates/id_dni/:id_dni", async (req, res) => {
+  const candidate = await Candidate.findOne({
+    id_dni: { string: req.params.id_dni },
+  });
+  res.send(candidate);
 });
 
 router.get("/candidates/:id", async (req, res) => {
