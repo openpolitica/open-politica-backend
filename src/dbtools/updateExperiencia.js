6f754563841fce4fb026e1569c46f753011a07f1
gianpietro1 <gianpietro1@gmail.com>
2021-01-16 15:50:30 -0500

    party alias & public exp improvements

--- src/dbtools/updateExperiencia.js
+++ src/dbtools/updateExperiencia.js
@@ -7,6 +7,7 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const results = [];
 
+console.log("Updating candidates with experience");
 fs.createReadStream("experiencia_candidate_level.csv")
   .pipe(csv())
   .on("data", (data) => results.push(data))
@@ -26,3 +27,24 @@ fs.createReadStream("experiencia_candidate_level.csv")
       })
     );
   });
+
+// fill the blanks
+const updateCandidates = async () => {
+  console.log("Updating candidates with no experience");
+  const candidates = await CandidateModel.find({});
+  await Promise.all(
+    candidates.map(async (candidateElement) => {
+      await CandidateModel.updateMany(
+        { experiencia_publica: null },
+        {
+          $set: {
+            experiencia_publica: false,
+          },
+        }
+      );
+    })
+  );
+  console.log("Finished updating candidates with no experience");
+};
+
+updateCandidates();
